830794b2f665790105178b045274d70544cdc822
Christian Arthur ONDAPHE <CON23863@open-groupe.com>
2024-04-05 23:58:08 +0200

    initialisation du projet

---
+++ src/main/java/cm/car/DealerCar.java
@@ -0,0 +1,13 @@
+package cm.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealerCar {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealerCar.class, args);
+	}
+
+}
---
+++ src/test/java/cm/car/DealerCar.java
@@ -0,0 +1,13 @@
+package cm.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealerCar {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
